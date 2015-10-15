e8c87ce7a55ecb9dad99ca307b17260a56a42b51
Reinhold Gschweicher <Reinhold.Gschweicher.fl@ait.ac.at>
2015-10-15 13:21:00 +0200

    Format clean_mysql_db.sql

--- clean_mysql_db.sql
+++ clean_mysql_db.sql
@@ -1,5 +1,17 @@
 -- sql dump --
-delete from relationship where startobject in (select id from object where deletets is not null) or endobject in (select id from object where deletets is not null); delete from attribute where object in (select id from object where deletets is not null); delete from sample where object in (select id from object where deletets is not null); delete from object where deletets is not null; delete from type where jevisclass not in (select name from objectclass); delete from classrelationship where startclass not in (select name from
-objectclass) or endclass not in (select name from objectclass);
+delete from relationship
+  where startobject in (select id from object where deletets is not null)
+    or endobject in (select id from object where deletets is not null);
+delete from attribute
+  where object in (select id from object where deletets is not null);
+delete from sample
+  where object in (select id from object where deletets is not null);
+delete from object
+  where deletets is not null;
+delete from type
+  where jevisclass not in (select name from objectclass);
+delete from classrelationship
+  where startclass not in (select name from objectclass)
+  or endclass not in (select name from objectclass);
 -- end --
 
